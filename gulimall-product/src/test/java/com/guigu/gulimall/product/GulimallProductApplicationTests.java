fb271f2fc1833bd66b1ff411b392e1a20fe03718
tom_glb <1903328094@qq.com>
2020-10-04 11:59:48 +0800

    redis的整合测试

--- gulimall-product/src/test/java/com/guigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/guigu/gulimall/product/GulimallProductApplicationTests.java
@@ -6,13 +6,33 @@ import com.guigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
 public class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void teststringRedisTemplate() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        //保存
+        ops.set("hello","world_"+ UUID.randomUUID().toString());
+
+        //查询
+        String s = ops.get("hello");
+
+        System.out.println("之前保存的数据是："+s);
+    }
+
 
     @Test
      public void contextLoads() {
